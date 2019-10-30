579230b3dd59e1d88505c56e9d9c0e1ebf127acb
usnaik <upendra_naik@yahoo.com>
2019-10-30 17:20:31 +0530

    first commit - kafka producers

---
+++ src/com/upen/kafka/AsynchronousProducer.java
@@ -0,0 +1,44 @@
+package com.upen.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class AsynchronousProducer {
+
+   public static void main(String[] args) throws Exception{
+      String topicName = "AsynchronousProducerTopic";
+          String key = "Key1";
+          String value = "Value-1";
+
+      Properties props = new Properties();
+      props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+      props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+      Producer<String, String> producer = new KafkaProducer <>(props);
+
+      ProducerRecord<String, String> record = new ProducerRecord<>(topicName,key,value);
+
+      producer.send(record, new MyProducerCallback());
+      System.out.println("AsynchronousProducer call completed");
+      producer.close();
+
+   }
+
+}
+
+    class MyProducerCallback implements Callback{
+
+       @Override
+       public  void onCompletion(RecordMetadata recordMetadata, Exception e) {
+        if (e != null)
+            System.out.println("AsynchronousProducer failed with an exception");
+                else
+                        System.out.println("AsynchronousProducer call Success:");
+       }
+   }
---
+++ src/com/upen/kafka/SimpleProducer.java
@@ -0,0 +1,29 @@
+package com.upen.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+public class SimpleProducer {
+  
+   public static void main(String[] args) throws Exception{
+           
+      String topicName = "SimpleProducerTopic";
+	  String key = "Key1";
+	  String value = "Value-1";
+      
+      Properties props = new Properties();
+      props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+      props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");         
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+	        
+      Producer<String, String> producer = new KafkaProducer <>(props);
+	
+	  ProducerRecord<String, String> record = new ProducerRecord<>(topicName,key,value);
+	  producer.send(record);	       
+      producer.close();
+	  
+	  System.out.println("SimpleProducer Completed.");
+   }
+}
