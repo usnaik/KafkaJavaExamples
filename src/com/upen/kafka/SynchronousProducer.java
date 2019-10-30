4ada09349da947a929594d33aff54768d12005f4
usnaik <upendra_naik@yahoo.com>
2019-10-30 19:05:10 +0530

    formatting

--- src/com/upen/kafka/SynchronousProducer.java
+++ src/com/upen/kafka/SynchronousProducer.java
@@ -6,32 +6,34 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+
 public class SynchronousProducer {
 
-   public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 
-      String topicName = "SynchronousProducerTopic";
-          String key = "Key1";
-          String value = "Value-1";
+		String topicName = "SynchronousProducerTopic";
+		String key = "Key1";
+		String value = "Value-1";
 
-      Properties props = new Properties();
-      props.put("bootstrap.servers", "localhost:9092,localhost:9093");
-      props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-      Producer<String, String> producer = new KafkaProducer <>(props);
+		Producer<String, String> producer = new KafkaProducer<>(props);
 
-          ProducerRecord<String, String> record = new ProducerRecord<>(topicName,key,value);
+		ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, value);
 
-      try{
-           RecordMetadata metadata = producer.send(record).get();
-           System.out.println("Message is sent to Partition no " + metadata.partition() + " and offset " + metadata.offset());
-           System.out.println("SynchronousProducer Completed with success.");
-      }catch (Exception e) {
-           e.printStackTrace();
-           System.out.println("SynchronousProducer failed with an exception");
-      }finally{
-           producer.close();
-      }
-   }
+		try {
+			RecordMetadata metadata = producer.send(record).get();
+			System.out.println(
+					"Message is sent to Partition no " + metadata.partition() + " and offset " + metadata.offset());
+			System.out.println("SynchronousProducer Completed with success.");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("SynchronousProducer failed with an exception");
+		} finally {
+			producer.close();
+		}
+	}
 }
