614840d0d6c26f58dea4e152fb18e244790f589a
usnaik <upendra_naik@yahoo.com>
2019-11-01 17:56:51 +0530

    updates

--- src/com/upen/kafka/rebalancelistner/ManualConsumer.java
+++ src/com/upen/kafka/rebalancelistner/ManualConsumer.java
@@ -1,4 +1,4 @@
-package com.upen.kafka.producer.supplier;
+package com.upen.kafka.rebalancelistner;
 
 import java.util.*;
 import java.io.*;
@@ -6,41 +6,39 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class SupplierConsumer {
+public class ManualConsumer {
 
 	public static void main(String[] args) throws Exception {
 
 		String topicName = "SupplierTopic";
 		String groupName = "SupplierTopicGroup";
+
 		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+		props.put("group.id", groupName);
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "SupplierDeserializer");
+		props.put("enable.auto.commit", "false");
 
-		// props.put("bootstrap.servers", "localhost:9092,localhost:9093");
-		// props.put("group.id", groupName);
-		// props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-		// props.put("value.deserializer", "SupplierDeserializer");
-
-		InputStream input = null;
 		KafkaConsumer<String, Supplier> consumer = null;
 
 		try {
-			input = new FileInputStream("SupplierConsumer.properties");
-			props.load(input);
 			consumer = new KafkaConsumer<>(props);
 			consumer.subscribe(Arrays.asList(topicName));
 
 			while (true) {
-				@SuppressWarnings("deprecation")
 				ConsumerRecords<String, Supplier> records = consumer.poll(100);
 				for (ConsumerRecord<String, Supplier> record : records) {
 					System.out.println("Supplier id= " + String.valueOf(record.value().getID()) + " Supplier  Name = "
 							+ record.value().getName() + " Supplier Start Date = "
 							+ record.value().getStartDate().toString());
 				}
+				consumer.commitAsync();
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
-			input.close();
+			consumer.commitSync();
 			consumer.close();
 		}
 	}
